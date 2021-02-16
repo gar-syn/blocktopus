03d135457b50c245b8e7274c698104a01914fa72
Rene <rene.ulrich@syngenta.com>
2021-02-16 14:09:23 +0100

    prepared for new branch v2-new-blocktopus

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,6 @@ from whitenoise import WhiteNoise
 from . import config
 from flask_login import LoginManager
 
-#db initilaization
 db = SQLAlchemy()
 
 def page_not_found(e):
@@ -30,7 +29,6 @@ def create_app():
 
     @login_manager.user_loader
     def load_user(user_id):
-        # since the user_id is just the primary key of our user table, use it in the query for the user
         return User.query.get(int(user_id))
 
     # blueprint for auth routes in our app
--- app/auth.py
+++ app/auth.py
@@ -22,17 +22,17 @@ def register_post():
     name = request.form.get('name')
     password = request.form.get('password')
 
-    # if this returns a user, then the email already exists in database
+    # check for existing user
     user = User.query.filter_by(email=email).first()
 
-    if user:  # if a user is found, we want to redirect back to signup page so user can try again
+    if user:
         flash('Email address is already registered')
         return redirect(url_for('auth.register'))
 
-    # create new user with the form data. Hash the password so plaintext version isn't saved.
+    # create new user
     new_user = User(email=email, name=name, password=generate_password_hash(password, method='sha256'))
 
-    # add the new user to the database
+    # add to database
     try:
         db.session.add(new_user)
         db.session.commit()
@@ -48,13 +48,11 @@ def login_post():
 
     user = User.query.filter_by(email=email).first()
 
-    # check if user actually exists
-    # take the user supplied password, hash it, and compare it to the hashed password in database
+    # check if user actually exists and compare passwords
     if not user or not check_password_hash(user.password, password):
         flash('Please check your login details and try again.')
         return redirect(url_for('auth.login'))  # if user doesn't exist or password is wrong, reload the page
 
-    # if the above check passes, then we know the user has the right credentials
     login_user(user, remember=remember)
     return redirect(url_for('main.profile'))
 
