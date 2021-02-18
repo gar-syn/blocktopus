049c51b97df03f5c0fd7e5dd8966d650c7c6e9bd
Rene <rene.ulrich@syngenta.com>
2021-02-18 13:03:02 +0100

    cleaned up static files folder structure

--- app/util/assets.py
+++ app/util/assets.py
@@ -4,17 +4,17 @@ bundles = {
     'main_css': Bundle(
         'css/dashboard.css',
         'css/style.css',
-        'fontawesome-free/css/all.css',
+        'css/lib/all.css',
         filters='cssmin',
         output='dist/css/style.min.css',
         extra={'rel': 'stylesheet/css'}),
 
     'main_js': Bundle(
-        'js/vendor/jquery/jquery.js',
-        'js/vendor/jquery-easing/jquery.easing.js',
-        'js/vendor/bootstrap/js/bootstrap.js',
-        'js/vendor/bootstrap/js/bootstrap.bundle.js',
-        'fontawesome-free/js/all.js',
+        'js/lib/jquery.js',
+        'js/lib/jquery.easing.js',
+        'js/lib/bootstrap.js',
+        'js/lib/bootstrap.bundle.js',
+        'js/lib/all.js',
         'js/dashboard.js',
         filters='jsmin',
         output='dist/js/main.min.js')
