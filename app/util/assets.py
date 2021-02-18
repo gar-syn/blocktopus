0b71640718c068500e259d2e4df82583ffd33899
Rene <rene.ulrich@syngenta.com>
2021-02-18 14:13:46 +0100

    changed static 'lib' name to 'libraries', so it wont be excluded by .gitignore file

--- app/util/assets.py
+++ app/util/assets.py
@@ -4,17 +4,17 @@ bundles = {
     'main_css': Bundle(
         'css/dashboard.css',
         'css/style.css',
-        'css/lib/all.css',
+        'css/libraries/all.css',
         filters='cssmin',
         output='gen/css/style.min.css',
         extra={'rel': 'stylesheet/css'}),
 
     'main_js': Bundle(
-        'js/lib/jquery.js',
-        'js/lib/jquery.easing.js',
-        'js/lib/bootstrap.js',
-        'js/lib/bootstrap.bundle.js',
-        'js/lib/all.js',
+        'js/libraries/jquery.js',
+        'js/libraries/jquery.easing.js',
+        'js/libraries/bootstrap.js',
+        'js/libraries/bootstrap.bundle.js',
+        'js/libraries/all.js',
         'js/dashboard.js',
         filters='jsmin',
         output='gen/js/main.min.js')
