2d607cc84b7c8ef1d6d711f8ed8dbbd6647ff953
Rene <rene.ulrich@syngenta.com>
2021-02-22 21:16:10 +0100

    cleaned up models class

--- app/models.py
+++ app/models.py
@@ -3,39 +3,6 @@ from sqlalchemy import Table, Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 
 from . import db
-from sqlalchemy.types import TypeDecorator, CHAR
-from sqlalchemy.dialects.postgresql import UUID
-import uuid
-
-class GUID(TypeDecorator):
-    impl = CHAR
-
-    def load_dialect_impl(self, dialect):
-        if dialect.name == 'postgresql':
-            return dialect.type_descriptor(UUID())
-        else:
-            return dialect.type_descriptor(CHAR(32))
-
-    def process_bind_param(self, value, dialect):
-        if value is None:
-            return value
-        elif dialect.name == 'postgresql':
-            return str(value)
-        else:
-            if not isinstance(value, uuid.UUID):
-                return "%.32x" % uuid.UUID(value).int
-            else:
-                # hexstring
-                return "%.32x" % value.int
-
-    def process_result_value(self, value, dialect):
-        if value is None:
-            return value
-        else:
-            # 20200328/PPM commented the next two lines to return the db id as string 
-            # if not isinstance(value, uuid.UUID):
-            #     value = uuid.UUID(value)
-            return value
 
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
