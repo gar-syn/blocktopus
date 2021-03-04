90a13388e35a228d4c2d9a49f09f5dc06cef0f38
Rene <rene.ulrich@syngenta.com>
2021-03-04 15:24:56 +0100

    added basic functional test for creating Flask App, initializing DB and checking home page, also added testing configuration to flask config file

--- app/tests/functional/test_main_view.py
+++ app/tests/functional/test_main_view.py
@@ -1,15 +1,45 @@
+import os
 import pytest
 
 from ... import create_app
+from ...util.extensions import db
+from ...models.model import User
+from ...util.config import TestConfig
 
-def test_home_page():
+
+
+@pytest.fixture(scope='module')
+def test_client():
+    flask_app = create_app(TestConfig)
+    testing_client = flask_app.test_client()
+    ctx = flask_app.app_context()
+    ctx.push()
+ 
+    yield testing_client
+ 
+    ctx.pop()
+    
+@pytest.fixture(scope='module')
+def init_database():
+    db.create_all()
+    user1 = User('Gary@proivder.tld','Gary', 'SuperStrongPassword123',  'CHMU', 'Building', 'Room')
+    user2 = User('Rene@proivder.tld','Rene', 'SuperStrongPassword321',  'CHMU', 'Building', 'Room')
+    db.session.add(user1)
+    db.session.add(user2)
+    db.session.commit()
+
+    yield db
+ 
+    db.drop_all()
+    
+def test_home_page(test_client):
     """
-    GIVEN a Flask application configured for testing
+    GIVEN a Flask application
     WHEN the '/' page is requested (GET)
-    THEN check that the response is valid
+    THEN check the response is valid
     """
-    flask_app = create_app()
-
-    with flask_app.test_client() as test_client:
-        response = test_client.get('/')
-        assert response.status_code == 200
+    response = test_client.get('/')
+    assert response.status_code == 200
+    assert b"Blocktopus Dashboard" in response.data
+    assert b"New Sketch" in response.data
+    assert b"Standard Library" in response.data
