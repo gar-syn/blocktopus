461f0372576dcbcd3e2cded308dfd6d57af6f14e
Rene <rene.ulrich@syngenta.com>
2021-03-08 08:40:36 +0100

    created functional tests for registration and login process and added testing db to .gitignore

--- app/tests/functional/test_auth.py
+++ app/tests/functional/test_auth.py
@@ -20,7 +20,6 @@ class ProjectTests(unittest.TestCase):
         with app.app_context():
             db.drop_all()
             db.create_all()
-
         
         self.assertEqual(app.debug, False)
  
@@ -35,10 +34,17 @@ class ProjectTests(unittest.TestCase):
     
     def register(self, email, password, name, site, building, room):
         return self.app.post(
-        'register/',
+        '/register',
         data=dict(email=email, password=password, name=name, site=site, building=building, room=room),
         follow_redirects=True
     )
+        
+    def login(self, email, password):
+        return self.app.post(
+            '/login',
+            data=dict(email=email, password=password),
+            follow_redirects=True
+        )
  
     ###############
     #### tests ####
@@ -56,9 +62,30 @@ class ProjectTests(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertIn(b'Sign up', response.data)
         self.assertIn(b'Register', response.data)
+        
+    def test_valid_user_registration(self):
+        self.app.get('/register', follow_redirects=True)
+        response = self.register('account@company.tld', 'SuperStrongPw123', 'myName', 'Site', 'Building', 'Room')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'Your account has been registered.', response.data)
+        self.assertIn(b'Please log in:', response.data)
+        
+    def test_valid_user_login(self):
+        self.app.get('/register', follow_redirects=True)
+        response = self.register('account@company.tld', 'SuperStrongPw123', 'myName', 'Site', 'Building', 'Room')
+        self.assertIn(b'Your account has been registered.', response.data)
+        response = self.login('account@company.tld', 'SuperStrongPw123')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'Your Profile', response.data)
+        self.assertIn(b'account@company.tld', response.data)
+        self.assertIn(b'myName', response.data)
+        self.assertIn(b'Change your Email', response.data)
 
+    def test_login_without_registering(self):
+        self.app.get('/login', follow_redirects=True)
+        response = self.login('unregistered_email@company.tld', 'NotExistingPassword123')
+        self.assertIn(b'Invalid username or password.', response.data)
+        self.assertIn(b'Please check your login credentials.', response.data)
 
 if __name__ == '__main__':
     unittest.main()
-    app = create_app(TestConfig)
-    app.run()
