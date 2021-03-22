32fe940eb2c0cc45661143d51c68b75f0daf48fa
Rene <rene.ulrich@syngenta.com>
2021-03-22 12:32:37 +0100

    added DB migration functionality to flask app by using Flask-Migrate and initializing it via flask script manager

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ from flask_assets import Environment
 from flask_login import LoginManager
 
 from app.util.config import configuration_classes
-from app.util.extensions import db, jsglue, bootstrap, create_celery_app, babel
+from app.util.extensions import db, jsglue, bootstrap, create_celery_app, babel, migrate
 
 def create_app(config_object='dev'):
     app = Flask(__name__)
@@ -50,6 +50,7 @@ def register_extensions(app):
     db.init_app(app)
     jsglue.init_app(app)
     babel.init_app(app)
+    migrate.init_app(app,db)
     #celery = create_celery_app(app)
     
 def register_loginmanager(app):
--- wsgi.py
+++ wsgi.py
@@ -1,11 +1,15 @@
 #Fask Entrypoint
 from app import create_app
-from app.util.extensions import db
+from app.util.extensions import db, migrate
 from app.util.logging import initialize_flask_logger
+from flask_script import Manager
+from flask_migrate import MigrateCommand
 
 app = create_app('dev')
 app.app_context().push()
 db.create_all()
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
     initialize_flask_logger(app)
