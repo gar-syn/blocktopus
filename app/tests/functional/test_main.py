1103111088f8b5df4c858f10aa9f4f7de45b6875
Rene <rene.ulrich@syngenta.com>
2021-03-26 09:11:12 +0100

    checked and formatted tests folder with flake8

--- app/tests/functional/test_main.py
+++ app/tests/functional/test_main.py
@@ -1,81 +1,78 @@
 import unittest
- 
+
 from app import create_app
-from app.util.extensions import db
-from app.models.model import User
- 
+
+
 class ProjectTests(unittest.TestCase):
- 
+
     ############################
     #### setup and teardown ####
     ############################
- 
+
     # executed prior to each test
     def setUp(self):
-        app = create_app('test')
+        app = create_app("test")
         self.app = app.test_client()
- 
+
         self.assertEqual(app.debug, False)
- 
+
     # executed after each test
     def tearDown(self):
         pass
- 
- 
+
     ########################
     #### helper methods ####
     ########################
- 
- 
- 
+
     ###############
     #### tests ####
     ###############
- 
+
     def test_main_page(self):
-        response = self.app.get('/', follow_redirects=True)
-        self.assertIn(b'Blocktopus Dashboard', response.data)
-        self.assertIn(b'New Sketch', response.data)
-        self.assertIn(b'Standard Library', response.data)
-        response = self.app.get('/home', follow_redirects=True)
-        self.assertIn(b'Blocktopus Dashboard', response.data)
-        self.assertIn(b'New Sketch', response.data)
-        self.assertIn(b'Standard Library', response.data)
-        response = self.app.get('/index', follow_redirects=True)
-        self.assertIn(b'Blocktopus Dashboard', response.data)
-        self.assertIn(b'New Sketch', response.data)
-        self.assertIn(b'Standard Library', response.data)
-        response = self.app.get('/index.html', follow_redirects=True)
-        self.assertIn(b'Blocktopus Dashboard', response.data)
-        self.assertIn(b'New Sketch', response.data)
-        self.assertIn(b'Standard Library', response.data)
+        response = self.app.get("/", follow_redirects=True)
+        self.assertIn(b"Blocktopus Dashboard", response.data)
+        self.assertIn(b"New Sketch", response.data)
+        self.assertIn(b"Standard Library", response.data)
+        response = self.app.get("/home", follow_redirects=True)
+        self.assertIn(b"Blocktopus Dashboard", response.data)
+        self.assertIn(b"New Sketch", response.data)
+        self.assertIn(b"Standard Library", response.data)
+        response = self.app.get("/index", follow_redirects=True)
+        self.assertIn(b"Blocktopus Dashboard", response.data)
+        self.assertIn(b"New Sketch", response.data)
+        self.assertIn(b"Standard Library", response.data)
+        response = self.app.get("/index.html", follow_redirects=True)
+        self.assertIn(b"Blocktopus Dashboard", response.data)
+        self.assertIn(b"New Sketch", response.data)
+        self.assertIn(b"Standard Library", response.data)
 
     def test_main_page_english(self):
-        self.app.get('/', follow_redirects=True)
-        response = self.app.get('/language/en', follow_redirects=True)
-        self.assertIn(b'New Sketch', response.data)
-        self.assertIn(b'Create...', response.data)
-        self.assertIn(b'English', response.data)
+        self.app.get("/", follow_redirects=True)
+        response = self.app.get("/language/en", follow_redirects=True)
+        self.assertIn(b"New Sketch", response.data)
+        self.assertIn(b"Create...", response.data)
+        self.assertIn(b"English", response.data)
 
     def test_main_page_german(self):
-        self.app.get('/', follow_redirects=True)
-        response = self.app.get('/language/de', follow_redirects=True)
-        self.assertIn(b'Neue Skizze', response.data)
-        self.assertIn(b'Erstelle...', response.data)
-        self.assertIn(b'Deutsch', response.data)
+        self.app.get("/", follow_redirects=True)
+        response = self.app.get("/language/de", follow_redirects=True)
+        self.assertIn(b"Neue Skizze", response.data)
+        self.assertIn(b"Erstelle...", response.data)
+        self.assertIn(b"Deutsch", response.data)
 
     def test_main_page_french(self):
-        self.app.get('/', follow_redirects=True)
-        response = self.app.get('/language/fr', follow_redirects=True)
-        self.assertIn(b'Nouvelle esquisse', response.data)
-        self.assertIn(b'Cr\xc3\xa9er...', response.data)
-        self.assertIn(b'Fran\xc3\xa7ais', response.data)
+        self.app.get("/", follow_redirects=True)
+        response = self.app.get("/language/fr", follow_redirects=True)
+        self.assertIn(b"Nouvelle esquisse", response.data)
+        self.assertIn(b"Cr\xc3\xa9er...", response.data)
+        self.assertIn(b"Fran\xc3\xa7ais", response.data)
 
     def test_404_error(self):
-        response = self.app.get('/not-existing-page', follow_redirects=True)
+        response = self.app.get("/not-existing-page", follow_redirects=True)
         self.assertEqual(response.status_code, 404)
-        self.assertIn(b'404', response.data)
-        self.assertIn(b'Page Not Found', response.data)
+        self.assertIn(b"404", response.data)
+        self.assertIn(b"Page Not Found", response.data)
+
 
 if __name__ == "__main__":
     unittest.main()
